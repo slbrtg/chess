8d4e7fce6c721767e850eb24f0b9dc1f0fdbf5cb
Saul Artiga <slbrtg@gmail.com>
2017-06-15 16:46:53 -0700

    begin to autocolor the board

--- js/scripts.js
+++ js/scripts.js
@@ -17,8 +17,59 @@ function Game (){
   this.row8.length = 8
 }
 
-function chessPiece (){
+function chessPiece(piece,movement){
   this.piece = piece;
   this.movement = movement;
-  this.teamColor = teamColor;
+  this.teamColor = "";
 }
+
+function Pawn(){
+  chessPiece.call(this,piece,movement,teamColor);
+  this.piece = "Pawn";
+  this.movement = function(){   //define pawn's movements
+  }
+}
+
+function Rook(){
+  chessPiece.call(this,piece,movement,teamColor);
+  this.piece = "Rook";
+  this.movement = function(){   //define pawn's movements
+  }
+}
+
+function Bishop(){
+  chessPiece.call(this,piece,movement,teamColor);
+  this.piece = "Bishop";
+  this.movement = function(){   //define pawn's movements
+  }
+}
+
+function Knight(){
+  chessPiece.call(this,piece,movement,teamColor);
+  this.piece = "Knight";
+  this.movement = function(){   //define pawn's movements
+  }
+}
+
+function Queen(){
+  chessPiece.call(this,piece,movement,teamColor);
+  this.piece = "Queen";
+  this.movement = function(){   //define pawn's movements
+  }
+}
+
+function King(){
+  chessPiece.call(this,piece,movement,teamColor);
+  this.piece = "King";
+  this.movement = function(){   //define pawn's movements
+  }
+}
+
+$(document).ready(function(){
+  for (var x = 0.5; x < 64; x = x + 0.5){
+    var jumpSquare = 1.5;
+    if (x % jumpSquare === 0){
+      $('#square'+x).css('background-color', 'green');
+    }
+  }
+});
